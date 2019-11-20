80d88296104ba9dcc5bd0c5e29b78fe089fca58b
Topher Pedersen <chrispedatx@gmail.com>
2019-11-20 14:36:20 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
+// MobX State Tree Quickstart Tutorial: https://github.com/mobxjs/mobx-state-tree/blob/master/docs/getting-started.md#getting-started
+// BOOKMARK: Pick back up with Quickstart Tutorial at "Snapshots are awesome!"
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { types, getSnapshot } from "mobx-state-tree"
+
+const Todo = types
+    .model({
+        name: types.optional(types.string, ""),
+        done: types.optional(types.boolean, false)
+    })
+    .actions(self => ({
+        setName(newName) {
+            self.name = newName
+        },
+
+        toggle() {
+            self.done = !self.done
+        }
+    }))
+
+const User = types.model({
+    name: types.optional(types.string, "")
+})
+
+const RootStore = types
+    .model({
+        users: types.map(User),
+        todos: types.optional(types.map(Todo), {})
+    })
+    .actions(self => ({
+        addTodo(id, name) {
+            self.todos.set(id, Todo.create({ name }))
+        }
+    }))
+
+const john = User.create({ name: "Johnny Wang" })
+const eat = Todo.create({ name: "Johnny Wang", done: false })
+
+// store.addTodo(1, "Eat a cake")
+// store.todos.get(1).toggle()
+
+console.log("John:", getSnapshot(john))
+console.log("Eat TODO:", getSnapshot(eat))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello, MobX State Tree</h1>
+        </div>
+    );
 }
 
 export default App;
